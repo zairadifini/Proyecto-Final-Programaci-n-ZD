c7acb46f7e9b6cfbad27b085ef8ecceef1c4e6e5
alumnos alumnos <alumnos@new.edu.ar>
2022-11-14 15:56:08 -0300

    Proyecto final

--- JuegoDeLaSerpienteZDF.py
+++ JuegoDeLaSerpienteZDF.py
@@ -194,8 +194,8 @@ def main():
  
         for x in range(len(s.body)):
             if s.body[x].pos in list(map(lambda z:z.pos,s.body[x+1:])):
-                print (\'Score:\ ', len (s.body))
-                message_box(\'You Lost!\', \'Play again...\')
+                print ('Score:  ', len (s.body))
+                message_box('You Lost!', 'Play again...')
                 s.reset((10,10))
                 break
  
